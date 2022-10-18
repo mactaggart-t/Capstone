4eea9521274e61556a0224a2d2633e526bc0ba55
Ethan Lam <lam.eth@northeastern.edu>
2022-10-18 17:26:13 -0400

    Removed need to input Primary Key manually for all tables, now auto increments

--- backendAPI/api.py
+++ backendAPI/api.py
@@ -11,9 +11,9 @@ conn = pymysql.connect(
         )
 
 # insert query into users table
-def insert_users(users_id, email, pwd, join_date, max_capacity, firstname, lastname):
+def insert_users(email, pwd, join_date, max_capacity, firstname, lastname):
     cur=conn.cursor()
-    cur.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s, %s)", (users_id, email, pwd, join_date, max_capacity, firstname, lastname))
+    cur.execute("INSERT INTO users (email, pwd, join_date, max_capacity, firstname, lastname) VALUES (%s, %s, %s, %s, %s, %s)", (email, pwd, join_date, max_capacity, firstname, lastname))
     conn.commit()
 
 # read the data from users table
@@ -23,17 +23,17 @@ def get_users():
     users = cur.fetchall()
     return users
 
-# delete row from users table based on id
-def delete_from_users(id):
+# delete row from users table based on email
+def delete_from_users(email):
     cur = conn.cursor()
-    cur.execute("DELETE FROM users WHERE users_id=%s", (id))
+    cur.execute("DELETE FROM users WHERE email=%s", (email))
     conn.commit()
     
 ### Current/Temperature/Voltage ###
 # insert query into a table
-def insert_data(table, session_id, time, data, users_id):
+def insert_data(table, time, data, user_id):
     cur=conn.cursor()
-    cur.execute("INSERT INTO " + table + " VALUES (%s, %s, %s, %s)", (session_id, time, data, users_id))
+    cur.execute("INSERT INTO " + table + " (time, " + table + ", user_id) VALUES (%s, %s, %s)", (time, data, user_id))
     conn.commit()
 
 # read the data from a table
@@ -43,47 +43,47 @@ def get_data(table):
     data = cur.fetchall()
     return data
 
-# delete row from a table based on session id
-def delete_data(table, session_id):
+# delete row from a table based on user id
+def delete_data(table, user_id):
     cur = conn.cursor()
-    cur.execute("DELETE FROM " + table + " WHERE session_id=%s", (session_id))
+    cur.execute("DELETE FROM " + table + " WHERE user_id=%s", (user_id))
     conn.commit()
 
 ### TESTING ###
 # inserts data into users
 print("Insert User Data into Table")
-insert_users('5', 'johndoe@capstone.com', 'fall', '2985-9-1', '1000', 'John', 'Doe')
+insert_users('johndoe@capstone.com', 'fall', '2985-9-1', '1000', 'John', 'Doe')
 print(get_users())
 # delete a row from users
 print("Delete User Data from Table")
-delete_from_users('5')
+delete_from_users('johndoe@capstone.com')
 print(get_users())
 
 # inserts data into current
 print("Insert Current Data into Table")
-insert_data('current', '1', '10', '80', '7')
+insert_data('current', '10', '80', '7')
 print(get_data('current'))
 # delete a row froms current
 print("Delete Current Data from Table")
-delete_data('current', '1')
+delete_data('current', '7')
 print(get_data('current'))
 
-# inserts data into temperature
+## inserts data into temperature
 print("Insert Temperature Data into Table")
-insert_data('temperature', '10', '120', '20', '6')
+insert_data('temperature', '120', '20', '6')
 print(get_data('temperature'))
 # delete a row froms temperature
 print("Delete Temperature Data from Table")
-delete_data('temperature', '10')
+delete_data('temperature', '6')
 print(get_data('temperature'))
 
 # inserts data into voltage
 print("Insert Voltage Data into Table")
-insert_data('voltage', '100', '5', '12', '7')
+insert_data('voltage', '5', '12', '7')
 print(get_data('voltage'))
 # delete a row froms voltage
 print("Delete Voltage Data from Table")
-delete_data('voltage', '100')
+delete_data('voltage', '7')
 print(get_data('voltage'))
 
 #app = Flask(__name__)
