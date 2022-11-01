eecbdc296d6594f44acb8c0019e27ee4d0d62b5f
Ethan Lam <lam.eth@northeastern.edu>
2022-11-01 17:14:51 -0400

    Removed database code from api.py and into database_interactions.py

--- backendAPI/api.py
+++ backendAPI/api.py
@@ -1,56 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
-import pymysql
 import json
-from database_interactions import create_new_session_and_add_data, add_raw_data_to
-
-
-conn = pymysql.connect(
-        host= 'capstone.cmkbscvpp696.us-east-1.rds.amazonaws.com',
-        port = 3306,
-        user = 'admin',
-        password = 'ChuckGroup3!',
-        db = 'default_app',
-        )
-
-# insert query into users table
-def insert_users(email, pwd, join_date, max_capacity, firstname, lastname):
-    cur=conn.cursor()
-    cur.execute("INSERT INTO users (email, pwd, join_date, max_capacity, firstname, lastname) VALUES (%s, %s, %s, %s, %s, %s)", (email, pwd, join_date, max_capacity, firstname, lastname))
-    conn.commit()
-
-# read the data from users table
-def get_users():
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM users")
-    users = cur.fetchall()
-    return users
-
-# delete row from users table based on email
-def delete_from_users(email):
-    cur = conn.cursor()
-    cur.execute("DELETE FROM users WHERE email=%s", (email))
-    conn.commit()
-    
-### Current/Temperature/Voltage ###
-# insert query into a table
-def insert_data(table, time, data, user_id):
-    cur=conn.cursor()
-    cur.execute("INSERT INTO " + table + " (time, " + table + ", user_id) VALUES (%s, %s, %s)", (time, data, user_id))
-    conn.commit()
-
-# read the data from a table
-def get_data(table):
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM " + table)
-    data = cur.fetchall()
-    return data
-
-# delete row from a table based on user id
-def delete_data(table, user_id):
-    cur = conn.cursor()
-    cur.execute("DELETE FROM " + table + " WHERE user_id=%s", (user_id))
-    conn.commit()
+from database_interactions import *
 
 app = Flask(__name__)
 api = Api(app)
--- backendAPI/database_interactions.py
+++ backendAPI/database_interactions.py
@@ -1,4 +1,64 @@
+# This file contains backend interaction with the database (AWS Server).
+import datetime
+import pymysql
 
+# Establish a conection with the database
+conn = pymysql.connect(
+        host= 'capstone.cmkbscvpp696.us-east-1.rds.amazonaws.com',
+        port = 3306,
+        user = 'admin',
+        password = 'ChuckGroup3!',
+        db = 'default_app',
+        )
+
+### USER TABLE ###
+# insert user data into users table, get the current time and date for joining, first and last names are optional
+def insert_user(email, pwd, firstname=None, lastname=None):
+    cur=conn.cursor()
+    now = datetime.datetime.now()
+    cur.execute("INSERT INTO users (email, pwd, join_date, firstname, lastname) VALUES (%s, %s, %s, %s, %s, %s)", (email, pwd, now, firstname, lastname))
+    conn.commit()
+
+# see all users in the user table
+def get_all_users():
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM users")
+    users = cur.fetchall()
+    return users
+    
+# see information for a specific user, based on their email
+def get_user(email):
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM users WHERE email=%s", (email))
+    user = cur.fetchall()
+    return user
+
+# delete row from users table based on email
+def delete_from_users(email):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM users WHERE email=%s", (email))
+    conn.commit()
+    
+### Current/Temperature/Voltage ###
+# insert query into a table
+def insert_data(table, time, data, user_id):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO " + table + " (time, " + table + ", user_id) VALUES (%s, %s, %s)", (time, data, user_id))
+    conn.commit()
+
+# read the data from a table
+def get_data(table):
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM " + table)
+    data = cur.fetchall()
+    return data
+
+# delete row from a table based on user id
+def delete_data(table, user_id):
+    cur = conn.cursor()
+    cur.execute("DELETE FROM " + table + " WHERE user_id=%s", (user_id))
+    conn.commit()
+    
 def create_new_session_and_add_data(total_voltage, temperature, voltage_one,
  voltage_two, current):
     return 1
